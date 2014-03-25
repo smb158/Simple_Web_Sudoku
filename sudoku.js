8059fe2b36c6ff8d91d10c67b1b4841178af951f
smb158 <smb158@pitt.edu>
2014-03-24 21:40:18 -0400

    final commit

---
+++ sudoku.js
@@ -0,0 +1,11 @@
+//
+// File: sudoku.js
+// URL:  http://db.cs.pitt.edu/courses/cs1520/fall2007/sudoku.js
+// Desc: Initial Board for JavaScript Sudoku assignment
+//
+var initBoard = new Array(new Array(2, 0, 0, 0),
+                          new Array(1, 0, 4, 0),
+                          new Array(3, 0, 0, 4),
+                          new Array(0, 0, 2, 3)
+);
+
